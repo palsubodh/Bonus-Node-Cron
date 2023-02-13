b96a63672094a895e433b780a7aa19842119e6d7
palsubodh <palsubodh390@gmail.com>
2023-02-13 21:18:48 +0530

    Done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express= require("express")
+const app = express()
+
+const cron = require("node-cron");
+const router = express. Router();
+const route=require("./file")
+ app.use("/",route)
+
+ app.listen(process.env.PORT || 3000, function(){
+    console.log("running at "+(process.env.PORT||3000))
+ })
